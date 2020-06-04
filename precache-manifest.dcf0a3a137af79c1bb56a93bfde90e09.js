efad53a3f7ad9cf7eb484476ef9004ebc35dad51
KENDRY USUARIO DANIEL <kendrysotomorales@gmail.com>
2020-06-03 21:37:19 -0500

    Updates

---
+++ precache-manifest.dcf0a3a137af79c1bb56a93bfde90e09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77a3f722cb1ef474ec4dc811c206ee1",
+    "url": "/News-room/index.html"
+  },
+  {
+    "revision": "121dd8c34a7ca42d4dd8",
+    "url": "/News-room/static/css/main.3fa68bc5.chunk.css"
+  },
+  {
+    "revision": "b3d53000d2dc88378eba",
+    "url": "/News-room/static/js/2.2711f4b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/News-room/static/js/2.2711f4b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "121dd8c34a7ca42d4dd8",
+    "url": "/News-room/static/js/main.2418001f.chunk.js"
+  },
+  {
+    "revision": "db1ef8c83ded398e81ab",
+    "url": "/News-room/static/js/runtime-main.d7bf2ea6.js"
+  }
+]);
